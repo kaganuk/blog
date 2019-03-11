2b3a3a624b02b52b626af28a4d835f4d06e3d29c
kaganuk <kagankilicli@gmail.com>
2019-03-11 11:54:35 +0300

    add login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path(r'signup/', views.signup, name='signup'),
+    path(r'signup/', views.signup_view, name='signup'),
+    path(r'login/', views.login_view, name='login'),
 ]
